d919ee6770d30f61040e644ab3b42f5575c62a7f
smkdh <smrkdeogks@gmail.com>
2019-04-30 00:11:18 +0900

    canvas path 라인 적용

---
+++ app.js
@@ -0,0 +1,47 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.0;
+
+let painting = false;
+
+function stopPainting(){
+	painting = false;
+}
+
+function startPainting(){
+	painting = true;
+}
+
+function onMouseMove(event){
+	//마우스 이벤트 확인
+	//console.log(event); 
+	
+	const x = event.offsetX;
+	const y = event.offsetY;
+	console.log(x,y);
+
+	if(!painting){
+		ctx.beginPath();
+		ctx.moveTo(x,y);
+	} else {
+		ctx.lineTo(x,y);
+		ctx.stroke();
+	}
+}
+
+function onMouseDown(event){
+	painting = true;
+}
+
+
+if(canvas) {
+	canvas.addEventListener("mousemove", onMouseMove);
+	canvas.addEventListener("mousedown", startPainting);
+	canvas.addEventListener("mouseup", stopPainting);
+	canvas.addEventListener("mouseleave", stopPainting);
+}
